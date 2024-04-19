55d82ad919db683b23cd1fed88f9de2681b5867f
paolo flore <paoloflore937@gmail.com>
2024-04-19 14:49:24 +0200

    creta la class

--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,16 +1,30 @@
-﻿namespace csharp_oop_shop
+﻿using csharp_oop_shop;
+
+namespace csharp_oop_shop
 {
 
     public class Prodotto
     {
-    
+        public int codice;
+        public string name;
+        public string description;
+        public double prezzo;
+        public int iva;
+
+
+        public  Prodottodettagliato
+        {
+
+        }
+
+
     }
 
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Prodotto Prodottodettagliato = new Prodotto();
         }
     }
 }
